4ba474e93f61ffb918082f78303262d350216d33
EdgarDaza <edgar.daza@utp.ac.pa>
2024-11-09 22:33:08 -0500

    Agregado el boletín, falta su programación

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,11 +46,14 @@ export default function Footer(){
             <h4>Contacto</h4>
             <p>Correo: panamainfo@mail.com</p>
             <p>Teléfono: 775-1723 </p>
+            </div>
+
+            <div className='col'>
             <h4>Subscribete para Noticias y Novedades</h4>
             <form>
               <input className="campo" type="text" name="name" placeholder="Nombre"></input>
               <input className="campo" type="email" name="email" placeholder="Correo"></input>
-              <input className="btn-2" type="text" value="enviar"></input>
+              <input className="btn-2" type="button" value="Enviar"></input>
             </form>
             </div>
 
